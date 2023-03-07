fb7519d77bd361b3c53963a4db3c35dbba33747d
VladaBolotova <vladislavabolotova@gmail.com>
2023-03-06 21:06:58 -0500

    editing projects page

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,11 +2,9 @@ import style from "./project.module.css"
 
 const Project = ({ name, description, git, link, image }) => {
     return (
-        <div className="container" style={{
-            backgroundImage: `url('${image}')`
-        }}>
-            <h4>{name}</h4>
-            <p>{description}</p>
+        <div>
+            <h4>{name}</h4> 
+             <p>{description}</p>
             <ul>
                 <li>
                     <a href={git}>git</a>
@@ -15,6 +13,12 @@ const Project = ({ name, description, git, link, image }) => {
                     <a href={link}>live</a>
                 </li>
             </ul>
+        <div className="container" style={{
+            backgroundImage: `url('${image}')`
+        }}>
+            
+          
+        </div>
         </div>
     )
 }
